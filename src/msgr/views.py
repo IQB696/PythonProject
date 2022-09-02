5eb92cca94159bda8ec6c5a2cfa936c9a48b3914
IQB696 <112216194+IQB696@users.noreply.github.com>
2022-09-02 23:44:21 +0300

    Fix language

--- src/msgr/views.py
+++ src/msgr/views.py
@@ -12,19 +12,27 @@ def about(request):
     return render(request, 'msgr/about.html')
 
 
+def __validate(form, str):
+    x = ('0', '1', '2')
+    for c in str:
+        if x.__contains__(c):
+            form.add_error('title', 'ERROR')
+
+
 def create(request):
     error = ''
     if request.method == 'POST':
         form = SmsForm(request.POST)
+        __validate(form, form.data.get('title'))
         if form.is_valid():
             form.save()
             return redirect('home')
-            error ='Error send'
         else:
             error ='Error send'
 
     form = SmsForm()
     context = {
-        'form': form
+        'form': form,
+        'error': error
     }
     return render(request, 'msgr/create.html', context)
